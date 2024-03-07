7e43c5069d58600853b3ef6e3bb0bca57c14fd93
kamalabot <kamaljp@gmail.com>
2024-03-07 15:51:43 +0530

    updated supporting app

--- grind75/probs_approach/supporting_app.py
+++ grind75/probs_approach/supporting_app.py
@@ -8,7 +8,7 @@ current_dir = pathlib.Path(__file__).parent.resolve()
 files_location = current_dir / "approach_prob.json"
 questions_data = current_dir / "QuestionGroups.json"
 files_location = files_location.resolve()
-print(files_location)
+# print(files_location)
 
 with open(files_location, 'r') as grind:
     approach_json = json.load(grind)
@@ -21,13 +21,16 @@ all_data = []
 for qd in q_vals:
     for x in qd:
         all_data.append(x)
-
-num_slider = st.slider('Selector',
-                       min_value=0,
-                       max_value=74)
-
-data = approach_json[num_slider]
-question = all_data[num_slider]
+all_slugs = [item['slug'] for item in all_data]
+# num_slider = st.slider('Selector',
+                       # min_value=0,
+                       # max_value=74)
+select_question = st.selectbox(label='Questions',
+                               options=all_slugs)
+get_slug_idx = all_slugs.index(select_question)
+# print(get_slug_idx)
+data = approach_json[get_slug_idx]
+question = all_data[get_slug_idx]
 
 st.markdown(f"### {question['slug']}")
 
