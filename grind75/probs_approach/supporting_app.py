02e764ff7ce40dda49b0419c9491ab6b69a957f3
kamalabot <kamaljp@gmail.com>
2024-03-07 16:23:00 +0530

    updated topics selector

--- grind75/probs_approach/supporting_app.py
+++ grind75/probs_approach/supporting_app.py
@@ -21,24 +21,49 @@ all_data = []
 for qd in q_vals:
     for x in qd:
         all_data.append(x)
-all_slugs = [item['slug'] for item in all_data]
+# print(type(approach_json))
+# bringing the approach and the challenge statement inside all_data
+for ind, vals in enumerate(approach_json):
+    all_data[ind]['challenge_statement'] = vals['challenge_statement']
+    all_data[ind]['approach'] = vals['approach']
+
+
+unique_topics = set([item['topic'] for item in all_data])
+topics_list = list(unique_topics)
+# print(topics_list)
+
+select_topic = st.selectbox(label='topic',
+                            options=topics_list,)
 # num_slider = st.slider('Selector',
                        # min_value=0,
                        # max_value=74)
+
+
+def filter_topic(item, tgt_topic):
+    if item['topic'] == tgt_topic:
+        return True
+
+
+filtered_data = [item for item in all_data if filter_topic(item, select_topic)]
+
+filtered_slugs = [item['slug'] for item in filtered_data]
 select_question = st.selectbox(label='Questions',
-                               options=all_slugs)
-get_slug_idx = all_slugs.index(select_question)
+                               options=filtered_slugs)
+
+
+get_slug_idx = filtered_slugs.index(select_question)
+
 # print(get_slug_idx)
-data = approach_json[get_slug_idx]
-question = all_data[get_slug_idx]
+# data = approach_json[get_slug_idx]
+question = filtered_data[get_slug_idx]
 
 st.markdown(f"### {question['slug']}")
 
 st.markdown("### Challenge Statement")
-st.write(data['challenge_statement'])
+st.write(question['challenge_statement'])
 
 st.markdown("### Leetcode Link")
 st.write(question["url"])
 
 with st.expander('See Approach', expanded=False):
-    st.write(data['approach'])
+    st.write(question['approach'])
